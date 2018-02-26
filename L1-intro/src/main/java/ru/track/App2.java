356e95bb14c6b9027e28415a5d8616f9cb584854
artem062 <artem_062@mail.ru>
2018-02-26 12:11:34 +0300

    http-client

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,12 +1,35 @@
 package ru.track;
 
+/**
+ * TASK:
+ * POST request to  https://guarded-mesa-31536.herokuapp.com/track
+ * fields: name,github,email
+ *
+ * LIB: http://unirest.io/java.html
+ *
+ *
+ */
 
-import org.apache.commons.lang3.StringUtils;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
 public class App2 {
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+    public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_GITHUB = "github";
+    public static final String FIELD_EMAIL = "email";
+
+    public static void main(String[] args) throws Exception {
+        HttpResponse<JsonNode> req = Unirest.post(URL)
+                .field(FIELD_NAME, "Voronov Artyom")
+                .field(FIELD_GITHUB, "artem062")
+                .field(FIELD_EMAIL, "artem_062@mail.ru")
+                .asJson();
+        boolean success = req.getBody().getObject().get("success").equals(true);
+        System.out.println(success);
+
     }
+
 }
