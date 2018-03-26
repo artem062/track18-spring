706ef2910ec87329c5f3a35e212dfb09d203ab12
artem062 <artem_062@mail.ru>
2018-03-26 13:37:02 +0300

    one more time

---
+++ L5_reflection/src/main/java/ru/track/json/JsonWriter.java
@@ -0,0 +1,163 @@
+package ru.track.json;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+/**
+ * сериализатор в json
+ */
+public class JsonWriter {
+
+    // В зависимости от типа объекта вызывает соответствующий способ сериализации
+    public static String toJson(@Nullable Object object) {
+        if (object == null) {
+            return "null";
+        }
+
+        Class clazz = object.getClass();
+
+        if (clazz.equals(String.class)
+                || clazz.equals(Character.class)
+                || clazz.isEnum()
+                ) {
+            return String.format("\"%s\"", object);
+        }
+
+        if (object instanceof Boolean || object instanceof Number) {
+            return object.toString();
+        }
+
+        if (clazz.isArray()) {
+            return toJsonArray(object);
+        }
+
+        if (object instanceof Collection) {
+            return toJsonCollection(object);
+        }
+
+        if (object instanceof Map) {
+            return toJsonMap(object);
+        }
+
+        return toJsonObject(object);
+    }
+
+    /**
+     * Используется вспомогательный класс {@link Array}, чтобы работать с object instanceof Array
+     * <p>
+     * То есть чтобы получить i-й элемент массива, нужно вызвать {@link Array#get(Object, int)}, где i - это число от 0 до {@link Array#getLength(Object)}
+     *
+     * @param object - который Class.isArray()
+     * @return строковое представление массива: [item1, item2, ...]
+     */
+    @NotNull
+    private static String toJsonArray(@NotNull Object object) {
+        int length = Array.getLength(object);
+        StringBuilder result = new StringBuilder();
+
+        result.append("[");
+        if (length != 0){
+            result.append(toJson(Array.get(object, 0)));
+            for (int i = 1; i < length; ++i){
+                result.append(',');
+                result.append(toJson(Array.get(object, i)));
+            }
+        }
+        result.append("]");
+
+        return result.toString();
+    }
+
+    /**
+     * В 1 шаг приводится к Collection
+     */
+    @NotNull
+    private static String toJsonCollection(@NotNull Object object) {
+        Collection collection = (Collection) object;
+        return toJsonArray(collection.toArray());
+    }
+
+    /**
+     * Сконвертить мап в json. Формат:
+     * {key:value, key:value,..}
+     * <p>
+     * На входе мы проверили, что это Map, можно просто кастовать Map map = (Map) object;
+     */
+    @NotNull
+    private static String toJsonMap(@NotNull Object object) {
+        // TODO: implement!
+        Map map = (Map) object;
+        Map<String, String> newMap = new LinkedHashMap<>();
+        List<Map.Entry> listMap = new LinkedList<>(map.entrySet());
+
+        for(Object obj : map.keySet()) {
+            if (map.get(obj) != null) {
+                newMap.put(obj.toString(), toJson(map.get(obj)));
+            }
+        }
+
+        return formatObject(newMap);
+        // Можно воспользоваться этим методом, если сохранить все поля в новой мапе уже в строковом представлении
+//        return formatObject(stringMap);
+    }
+
+    /**
+     * 1) Чтобы распечатать объект, нужно знать его внутреннюю структуру, для этого нужно получить его Class-объект:
+     * {@link Class} с помощью {@link Object#getClass()}
+     * <p>
+     * Получить поля класса можно с помощью {@link Class#getDeclaredFields()}
+     * Приватные поля недоступны, нужно изменить в рантайм их accessibility: {@link Field#setAccessible(boolean)}
+     * <p>
+     * 2) Вторая часть задачи: {@link JsonNullable} и {@link SerializedTo}
+     * Нужно проверить, что у класса/поля есть аннотация
+     * <p>
+     * {@link Class#getAnnotation(Class)} / {@link Field#getAnnotation(Class)}
+     * и в зависимости от этого изменить поведение
+     * <p>
+     * NOTE: Удобно сложить все поля объекта в Map<String, String> то етсь {имя поля -> значение поля в json}
+     * и воспользоваться методом {@link #formatObject(Map)}
+     */
+    @NotNull
+    private static String toJsonObject(@NotNull Object object) {
+
+        Class clazz = object.getClass();
+        Map<String, String> fields = new LinkedHashMap<>();
+
+        for (Field field : clazz.getDeclaredFields()){
+            field.setAccessible(true);
+            try {
+                if (field.get(object) != null || clazz.getAnnotation(JsonNullable.class) != null) {
+                    if (field.getAnnotation(SerializedTo.class) != null)
+                        fields.put(field.getAnnotation(SerializedTo.class).value(), toJson(field.get(object)));
+                    else
+                        fields.put(field.getName(), toJson(field.get(object)));
+                }
+            } catch (IllegalAccessException e) {}
+        }
+
+        return formatObject(fields);
+    }
+
+    /**
+     * Вспомогательный метод для форматирования содержимого Map<K, V>
+     *
+     * @param map
+     * @return "{key:value, key:value,..}"
+     */
+    @NotNull
+    private static String formatObject(@NotNull Map<String, String> map) {
+        String r = String.join(",", map.entrySet().stream()
+                .map(e -> String.format("\"%s\":%s", e.getKey(), e.getValue()))
+                .collect(Collectors.toList())
+        );
+
+        return String.format("{%s}", r);
+    }
+
+}
